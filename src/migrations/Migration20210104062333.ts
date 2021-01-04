a0c95562aa285c9730f2421a66f147f67a866c98
Remi <ghostlinksc@gmail.com>
2021-01-04 17:24:39 +1100

    Hashed user password with Argon2 1:21:52

--- src/migrations/Migration20210104062333.ts
+++ src/migrations/Migration20210104062333.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20201231062609 extends Migration {
+export class Migration20210104062333 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table "user" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "user_name" text not null, "password" text not null);');
