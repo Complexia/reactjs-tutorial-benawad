c7375a2f17d4ab54662622353a4ece5934f04280
Remi <ghostlinksc@gmail.com>
2021-01-05 16:52:15 +1100

    Implemented login 1:41:47

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 @InputType()
@@ -11,20 +11,104 @@ class UsernamePasswordInput {
     password: string
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string
+    @Field()
+    message: string
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[]
+    @Field(() => User, { nullable: true })
+    user?: User
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() {em}: MyContext
-    ) {
+    ): Promise<UserResponse> {
 
+        if(options.username.length <= 2) {
+            return {
+                errors:[{
+                    field: "username",
+                    message: "username too short"
+                }]
+            }
+        }
+
+        if(options.password.length <= 2) {
+            return {
+                errors:[{
+                    field: "password",
+                    message: "password too short"
+                }]
+            }
+        }
         const hashedPassword = await argon2.hash(options.password)
         const user = em.create(User, {
-             userName: options.username, 
+             username: options.username, 
              password: hashedPassword 
         })
-        await em.persistAndFlush(user)
-        return user 
+        try{
+            await em.persistAndFlush(user)
+        }
+        catch(err) {
+            //duplicate username error
+            if(err.code === "23505") {
+                return {
+                    errors: [{
+                        field: "username",
+                        message: "this username already exists"
+                    }]
+                }
+            }
+
+        }
+        return {
+            user
+        } 
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() {em}: MyContext
+    ):Promise<UserResponse> {
+
+        const user = await em.findOne(User, { username: options.username.toLowerCase() })
+
+        if(!user) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "no such user"
+                }]
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password)
+
+        if(!valid) {
+            return {
+                errors: [{
+                    field: "password",
+                    message: "wrong password"
+                }]
+            }
+        }
+        
+         
+        return {
+            user
+        }
+             
     }
 }
