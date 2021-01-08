317f75e88da47ddb12f05a2cbf0bb96cc550ed4d
Remi <ghostlinksc@gmail.com>
2021-01-08 15:57:20 +1100

    Implemented front end with Charka and Nextjs, 2:32:37, reorganized folder structure

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Container } from '../components/Container'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+
+
+const Index = () => (
+  <Container>
+
+    <div>
+      Hello World
+
+    </div>
+   <DarkModeSwitch /> 
+  </Container>
+ 
+ 
+)
+
+export default Index
